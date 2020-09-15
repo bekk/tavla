b58ecb5a3f404c55fbc0dfb07b433d72a5e5c0b0
3l-3m1n <el-emin.fejzagic@bekk.no>
2020-09-15 14:51:55 +0200

    busstopProto

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -9,6 +9,7 @@ import '../firebase-init'
 import Compact from '../dashboards/Compact'
 import Chrono from '../dashboards/Chrono'
 import Timeline from '../dashboards/Timeline'
+import BusStop from '../dashboards/BusStop'
 
 import LandingPage from './LandingPage'
 import Admin from './Admin'
@@ -38,6 +39,8 @@ function getDashboardComponent(
             return Timeline
         case 'Chrono':
             return Chrono
+        case 'BusStop':
+            return BusStop
         default:
             return Compact
     }
